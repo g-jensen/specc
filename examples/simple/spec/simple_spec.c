542341b11dd5989ffb94f2b04b092fa0e400291b
g-jensen <greg.micah.jensen@gmail.com>
2025-07-04 13:42:09 +0200

    fix some formatting

--- examples/simple/spec/simple_spec.c
+++ examples/simple/spec/simple_spec.c
@@ -7,117 +7,117 @@ module(simple, {
     
     it("this is what a passing test looks like",{
       should(1 == 1);
-    });
+    })
 
     it("failing boolean test",{
       should(1 == 0);
-    });
+    })
 
     it("failing char test", {
       should_eq('a','b',char);
-    });
+    })
 
     it("failing int test", {
       should_eq(1,2,int);
-    });
+    })
 
     it("failing float test", {
       should_eq(0.9f,2.4f,float);
-    });
+    })
 
     it("failing pointer test", {
       int a = 5;
       int b = 5;
       should_eq(&a,&b,int*);
-    });
+    })
 
     it("passing double equality", {
       should_double_eq(4.0,3.9,0.1);
-    });
+    })
 
     it("passing using my_function()", {
       should_eq(5,my_function(),int);
-    });
+    })
 
     it("pass followed by a fail", {
       should_eq(2,2,int);
       should_eq(5,2,int);
-    });
+    })
 
     it("fail followed by a pass", {
       should_eq(5,2,int);
       should_eq(2,2,int);
-    });
+    })
 
     it("multiple failures", {
       should_eq(1,2,int);
       should_eq(3,2,int);
-    });
+    })
 
     context("should_str_not_eq", {
       it("passes", {
         should_str_not_eq("hi","bye");
-      });
+      })
 
       it("fails", {
         should_str_not_eq("hi","hi");
-      });
-    });
+      })
+    })
 
     context("should_double_not_eq", {
       it("passes", {
         should_double_not_eq(0.f,10.f,1.f);
-      });
+      })
 
       it("fails", {
         should_double_not_eq(0.f,0.5f,1.f);
-      });
-    });
+      })
+    })
 
     context("context in middle", {
       it("failing pointer test", {
         int a = 5;
         int b = 5;
         should_eq(&a,&b,int*);
-      });
+      })
 
       context("nested context", {
         it("failing pointer test", {
           int a = 5;
           int b = 5;
-        should_eq(&a,&b,int*);
-        });
+          should_eq(&a,&b,int*);
+        })
 
         it("passing double equality", {
           should_double_eq(4.0,3.9,0.1);
-        });
-      });
+        })
+      })
 
       it("passing double equality", {
         should_double_eq(4.0,3.9,0.1);
-      });
-    });
+      })
+    })
 
     it("failing double equality", {
       should_double_eq(4.0,3.9,0.05);
-    });
+    })
 
     it("passing double inequality", {
       should_double_not_eq(4.0,3.9,0.05);
-    });
+    })
 
     it("failing string test", {
       should_str_eq("hello","bye");
-    });
+    })
 
     context("new context", {
       it("passing double inequality", {
         should_double_not_eq(4.0,3.9,0.05);
-      });
+      })
 
       it("failing string test", {
         should_str_eq("hello","bye");
-      });
-    });
-  });
-});
+      })
+    })
+  })
+})
--- examples/string/spec/string_spec.c
+++ examples/string/spec/string_spec.c
@@ -1,121 +1,149 @@
 #include "../headers/string.h"
 #include "../../../src/specc.h"
 
-module(string,
-    describe("string_charAt",
-
-        it("gets first index",
-            should_eq('h',string_charAt("hello",0),char))
-        
-        it("gets second index",
-            should_eq('y',string_charAt("bye",1),char)));
-
-    describe("string_length",
+module(string, {
+  describe("string helpers", {
     
-        it("zero length",
-            should_eq(0,string_length(""),int))
-            
-        it("length 1",
-            should_eq(1,string_length("a"),int))
-        
-        it("length 3",
-            should_eq(3,string_length("abc"),int)));
+    context("string_charAt",{
 
-    describe("string_compareTo",
-                    
-        it("compares to empty string", {
-            should_eq(0,string_compareTo("",""),int);
-            should_eq(1,string_compareTo("a",""),int);
-            should_eq(-1,string_compareTo("","a"),int);
-        });
+      it("gets first index",{
+        should_eq('h',string_charAt("hello",0),char);
+      })
         
-        it("compares with first char different", {
-            should_eq(25,string_compareTo("z","a"),int);
-            should_eq(23,string_compareTo("xbc","ae"),int);
-        });
-            
-        it("compares with second char different",
-            should_eq(-1,string_compareTo("ab","ac"),int))
-            
-        it("compares with third char different",
-            should_eq(3,string_compareTo("abi","abf"),int))
-            
-        it("compares exact same strings",
-            should_eq(0,string_compareTo("hello there","hello there"),int)
-            should_eq(0,string_compareTo("bye bye","bye bye"),int)));
+      it("gets second index",{
+        should_eq('y',string_charAt("bye",1),char);
+      })
+    })
 
-    describe("string_equals",
-        
-        it("empty strings are equal",
-            should(string_equals("","")))
-            
-        it("strings of different lengths are not equal",
-            should_not(string_equals("a",""))
-            should_not(string_equals("a","ab")))
-            
-        it("strings with different characters are not equal",
-            should_not(string_equals("mkx","abc"))
-            should_not(string_equals("c.b dx","c!b dx")))
-        
-        it("strings with same characters are equal",
-            should(string_equals("abc","abc"))
-            should(string_equals("c!b dx","c!b dx"))));
+    context("string_length",{
 
-    describe("string_copy",
+      it("zero length",{
+        should_eq(0,string_length(""),int);
+      })
+            
+      it("length 1",{
+        should_eq(1,string_length("a"),int);
+      })
         
-        it("copies empty string",
-            char* out = string_copy("");
-            should_str_eq("",out);
-            free(out);)
-            
-        it("copies populated string",
-            char* out = string_copy("h");
-            should_str_eq("h",out);
-            free(out);
-            
-            out = string_copy("hi");
-            should_str_eq("hi",out);
-            free(out);
-            
-            out = string_copy("bye");
-            should_str_eq("bye",out);
-            free(out);));
+      it("length 3",{
+          should_eq(3,string_length("abc"),int);
+      })
+    })
 
-    describe("string_toLowerCase",
-        
-        it("does nothing to lowercase string",
-            char* out = string_toLowerCase("");
-            should_str_eq("",out);
-            free(out);
-            
-            out = string_toLowerCase("a");
-            should_str_eq("a",out);
-            free(out);)
-            
-        it("converts uppcase to lowercase",
-            char* out = string_toLowerCase("A");
-            should_str_eq("a",out);
-            free(out);
-            
-            out = string_toLowerCase("Ab");
-            should_str_eq("ab",out);
-            free(out);
-            
-            out = string_toLowerCase("AB");
-            should_str_eq("aB",out);
-            free(out);));
+    context("string_compareTo",{
 
-    describe("string_compareToIgnoreCase",
+      it("compares to empty string", {
+        should_eq(0,string_compareTo("",""),int);
+        should_eq(1,string_compareTo("a",""),int);
+        should_eq(-1,string_compareTo("","a"),int);
+      })
+      
+      it("compares with first char different", {
+        should_eq(25,string_compareTo("z","a"),int);
+        should_eq(23,string_compareTo("xbc","ae"),int);
+      })
+          
+      it("compares with second char different",{
+        should_eq(-1,string_compareTo("ab","ac"),int);
+      })
+          
+      it("compares with third char different", {
+        should_eq(3,string_compareTo("abi","abf"),int);
+      })
+          
+      it("compares exact same strings", {
+        should_eq(0,string_compareTo("hello there","hello there"),int);
+        should_eq(0,string_compareTo("bye bye","bye bye"),int);
+      })
+    })
+
+    context("string_equals",{
+    
+      it("empty strings are equal",{
+        should(string_equals("",""));
+      })
+          
+      it("strings of different lengths are not equal",{
+        should_not(string_equals("a",""));
+        should_not(string_equals("a","ab"));
+      })
+          
+      it("strings with different characters are not equal",{
+        should_not(string_equals("mkx","abc"));
+        should_not(string_equals("c.b dx","c!b dx"));
+      })
+      
+      it("strings with same characters are equal",{
+        should(string_equals("abc","abc"));
+        should(string_equals("c!b dx","c!b dx"));
+      })
+    })
+
+    context("string_copy",{
         
-        it("has normal compareTo behavior for strings of same case",
-            should_eq(0,string_compareToIgnoreCase("",""), int);
-            should_eq(1,string_compareToIgnoreCase("a",""), int);
-            should_eq(-1,string_compareToIgnoreCase("","a"), int);
-            should_eq(3,string_compareToIgnoreCase("abi","abf"), int);
-            should_eq(3,string_compareToIgnoreCase("ABI","ABF"), int);)
+      it("copies empty string",{
+        char* out = string_copy("");
+        should_str_eq("",out);
+        free(out);
+      })
             
-        it("ignores case",
-            should_eq(3,string_compareToIgnoreCase("abi","Abf"), int);
-            should_eq(3,string_compareToIgnoreCase("abi","ABF"), int);
-            should_eq(0,string_compareToIgnoreCase("bye BYE","BYE bye"), int);));
-);
+      it("copies populated string",{
+        char* out = string_copy("h");
+        should_str_eq("h",out);
+        free(out);
+        
+        out = string_copy("hi");
+        should_str_eq("hi",out);
+        free(out);
+        
+        out = string_copy("bye");
+        should_str_eq("bye",out);
+        free(out);
+      })
+    })
+
+    context("string_toLowerCase",{
+        
+      it("does nothing to lowercase string",{
+        char* out = string_toLowerCase("");
+        should_str_eq("",out);
+        free(out);
+        
+        out = string_toLowerCase("a");
+        should_str_eq("a",out);
+        free(out);
+      })
+            
+      it("converts uppcase to lowercase",{
+        char* out = string_toLowerCase("A");
+        should_str_eq("a",out);
+        free(out);
+        
+        out = string_toLowerCase("Ab");
+        should_str_eq("ab",out);
+        free(out);
+        
+        out = string_toLowerCase("AB");
+        should_str_eq("aB",out);
+        free(out);
+      })
+    })
+
+    context("string_compareToIgnoreCase",{
+        
+      it("has normal compareTo behavior for strings of same case",{
+        should_eq(0,string_compareToIgnoreCase("",""), int);
+        should_eq(1,string_compareToIgnoreCase("a",""), int);
+        should_eq(-1,string_compareToIgnoreCase("","a"), int);
+        should_eq(3,string_compareToIgnoreCase("abi","abf"), int);
+        should_eq(3,string_compareToIgnoreCase("ABI","ABF"), int);
+      })
+            
+      it("ignores case",{
+        should_eq(3,string_compareToIgnoreCase("abi","Abf"), int);
+        should_eq(3,string_compareToIgnoreCase("abi","ABF"), int);
+        should_eq(0,string_compareToIgnoreCase("bye BYE","BYE bye"), int);
+      })
+    })
+  })
+})
